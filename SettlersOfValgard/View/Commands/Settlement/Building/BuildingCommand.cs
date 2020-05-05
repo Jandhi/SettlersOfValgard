3d481a980dca1193d379b586378b994635216dce
Jan Dohring <jandohring@hotmail.com>
2020-05-05 19:51:30 -0400

    Do building command

--- SettlersOfValgard/View/Commands/Settlement/Building/BuildingCommand.cs
+++ SettlersOfValgard/View/Commands/Settlement/Building/BuildingCommand.cs
@@ -1,7 +1,23 @@
-﻿namespace SettlersOfValgard.View.Commands.Settlement.Building
+﻿using System.Collections.Generic;
+using SettlersOfValgard.View.Commands.Core;
+
+namespace SettlersOfValgard.View.Commands.Settlement.Building
 {
-    public class BuildingCommand
+    public class BuildingCommand : ListOrDisplayCommand<Model.Building.Building>
     {
-        
+        public override string Name => "Building";
+        public override string[] Aliases { get; } = {"b"};
+        public override string UseCommandTo => "List buildings in your settlement, or display";
+        public override string Type => "Building";
+
+        public override List<Model.Building.Building> GetList(Game game)
+        {
+            return game.Settlement.Buildings;
+        }
+
+        public override void Display(Model.Building.Building t)
+        {
+            //TODO
+        }
     }
 }
